2babb039c6c66a6190da6033a9d1f52d54d517a8
root <root@9baf03f58c04>
2016-04-21 09:57:01 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "cool-blue",
+  "defaultLanguage" : "en",
+  "displayName" : "usingWaveKArtPS",
+  "homePage" : "Main",
+  "name" : "usingwavekartps",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
