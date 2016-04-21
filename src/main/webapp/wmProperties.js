52c13999d2ee1c1ffae87749efbc1bf15853fd0a
sushma test null <sushma.maddirala@testing.com>
2016-04-21 09:58:28 +0000

    New files added - [variables.less,style.css,theme.png,glyphicons-halflings-regular.woff2,.wmproject.properties,glyphicons-halflings-regular.eot,style.less,config.json,glyphicons-halflings-regular.ttf,glyphicons-halflings-regular.woff,glyphicons-halflings-regular.svg,gray-orange] Files modified - [Main.variables.json,index.html,eshopping.properties,.wmproject.properties,page.min.html,wmProperties.js,app.variables.json] Refactored files - [] Fixed bugs - [] Removed files - [sourcesanspro-regular.ttf,glyphicons-halflings-regular.woff,variables.less,style.less,sourcesanspro-regular.eot,.wmproject.properties,glyphicons-halflings-regular.eot,theme.png,sourcesanspro-regular.woff2,sourcesanspro-regular.woff,eshopping.log,glyphicons-halflings-regular.svg,glyphicons-halflings-regular.woff2,style.css,glyphicons-halflings-regular.ttf,sourcesanspro-regular.svg]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,5 +1,5 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "cool-blue",
+  "activeTheme" : "gray-orange",
   "defaultLanguage" : "en",
   "displayName" : "usingWaveKArtPS",
   "homePage" : "Main",
